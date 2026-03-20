f44f721da313ac0ffc97acbc188a82758b595d7e
Craig Bailie <Craig.Bailie0337@stu.mtec.edu>
2026-03-20 08:01:05 -0600

    First commit

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,41 @@
+import { $ } from '@wdio/globals'
+import Page from './page.js';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get getUserName () {
+        return $('#user-name');
+    }
+
+    get getPassword () {
+        return $('#password');
+    }
+
+    get submitButton () {
+        return $('#login-button');
+    }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    async login (username, password) {
+        await this.getUserName.setValue(username);
+        await this.getPassword.setValue(password);
+        await this.submitButton.click();
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open () {
+        return super.open('login');
+    }
+}
+
+export default new LoginPage();
